66f3d867aa759131799913683a34a684ab36cf5e
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-02-22 01:26:09 +0300

    добавлены обработчики первого раздела и окончания опроса

--- botHandlers.py
+++ botHandlers.py
@@ -9,22 +9,45 @@ import botMessageProvider
 import botStates
 import helpers
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def start_state_handler(update: Update, context: CallbackContext) -> int:
+    # TODO: проверить, проходил ли юзер раньше этот опрос. если проходил то надо предупредить что ответы перезапишутся
+
     text = botMessageProvider.get_start_state_text()
     reply_keyboard = [['Начнем']]
-    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard)
+    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     update.message.reply_text(text, reply_markup=keyboard_markup)
     return botStates.START_STATE
+
+
+def survey_finish_handler(update: Update, context: CallbackContext) -> int:
+    text = botMessageProvider.get_survey_finish_state_text()
+    reply_keyboard = [['Да', 'Нет']]
+    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    update.message.reply_text(text, reply_markup=keyboard_markup)
+    return botStates.SURVEY_FINISH_STATE
+
+
+def plans_info_handler(update: Update, context: CallbackContext) -> int:
+    text = botMessageProvider.get_plans_info_state_text()
+    update.message.reply_text(text, parse_mode='MarkdownV2')
+    return botStates.PLANS_INFO_STATE
+
+
+def total_finish_handler(update: Update, context: CallbackContext) -> int:
+    text = 'Теперь точно всё :) Спасибо и до скорых встреч!'
+    update.message.reply_text(text)
+    return botStates.TOTAL_FINISH_STATE
 
 
 def question_1_handler(update: Update, context: CallbackContext) -> int:
     text = botMessageProvider.get_question_1_state_text()
     reply_keyboard = [['1', '2'], ['3', 'Больше трех']]
-    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard)
+    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     update.message.reply_text(text, reply_markup=keyboard_markup)
     return botStates.QUESTION_1_STATE
 
@@ -45,7 +68,7 @@ def question_2_handler(update: Update, context: CallbackContext) -> int:
 
     text = botMessageProvider.get_question_2_state_text()
     reply_keyboard = [['Нет, не изучаю', 'Да, изучаю']]
-    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard)
+    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     update.message.reply_text(text, reply_markup=keyboard_markup)
     return botStates.QUESTION_2_STATE
 
@@ -74,25 +97,46 @@ def fork_handler(update: Update, context: CallbackContext) -> int:
 def part_1_question_1_handler(update: Update, context: CallbackContext) -> int:
     text = botMessageProvider.get_part_1_question_1_state_text()
     reply_keyboard = [['Да', 'Нет']]
-    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard)
+    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     update.message.reply_text(text, reply_markup=keyboard_markup)
-    return botStates.FORK_STATE
+    return botStates.PART_1_QUESTION_1_STATE
+
+
+def part_1_question_2_handler(update: Update, context: CallbackContext) -> int:
+    text = 'Где *ищете перевод* иностранного слова?'
+    update.message.reply_text(text, parse_mode='MarkdownV2')
+    return botStates.PART_1_QUESTION_2_STATE
+
+
+def part_1_question_3_handler(update: Update, context: CallbackContext) -> int:
+    text = 'Бывает так что правильное значение не найти сразу? Как убеждаетесь в правильном смысле?'
+    update.message.reply_text(text)
+    return botStates.PART_1_QUESTION_3_STATE
+
+
+def part_1_end_handler(update: Update, context: CallbackContext) -> int:
+    text = 'Эх, тогда вопросов больше нет :) Расскажите вообще что думаете про изучение иностранных языков?'
+    update.message.reply_text(text)
+    return botStates.PART_1_END_STATE
 
 
 def part_2_question_1_handler(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text('Для чего учите новый язык?')
+    text = 'Для чего учите новый язык?'
+    update.message.reply_text(text)
     return botStates.FORK_STATE
 
 
 def part_3_question_1_handler(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text('Какие инструменты использовали для запоминания новых слов?')
+    text = 'Какие инструменты использовали для запоминания новых слов?'
+    update.message.reply_text(text)
     return botStates.FORK_STATE
 
 
 def part_4_question_1_handler(update: Update, context: CallbackContext) -> int:
     # TODO: а кнопка "свой вариант ответа" где? и как сделать?
+    text = 'Какой язык сейчас учите?'
     reply_keyboard = [['Английский', 'Немецкий', 'Нидерландский',
-                      'Французский', 'Итальянский', 'Португальский', 'Испанский']]
-    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard)
-    update.message.reply_text('Какой язык сейчас учите?', reply_markup=keyboard_markup)
+                       'Французский', 'Итальянский', 'Португальский', 'Испанский']]
+    keyboard_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    update.message.reply_text(text, reply_markup=keyboard_markup)
     return botStates.FORK_STATE
