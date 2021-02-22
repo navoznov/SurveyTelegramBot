f1e8cfedb333bff65860aa9d0523e396453ec7ff
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-02-22 11:51:21 +0300

    добавлен метод сохраняющий пустой ответ

--- botMessageSaver.py
+++ botMessageSaver.py
@@ -1,7 +1,7 @@
 from telegram import Message
 from typing import Dict, Optional
 
-def save_answer(message: Message, user_data:Optional[Dict], part: int, question_id: str):
+def save_answer(message: Message, user_data:Optional[Dict], part: int, question_id: str) -> None:
     if not message.voice is None:
         VOICE_METADATA_SEPARATOR = '***'
         print(message.voice.mime_type)
@@ -9,3 +9,6 @@ def save_answer(message: Message, user_data:Optional[Dict], part: int, question_
     else:
         user_data[question_id] = message.text
     pass
+
+def set_empty_answer(user_data:Optional[Dict], part: int, question_id: str) -> None:
+    user_data[question_id] = ''
